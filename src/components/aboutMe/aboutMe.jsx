0a8e7a26ebad67af95ac72af71c726d60ffe37dd
Taposh <taposhbarman422@gmail.com>
2024-08-24 15:02:37 +0600

    ui update

--- src/components/aboutMe/aboutMe.jsx
+++ src/components/aboutMe/aboutMe.jsx
@@ -38,7 +38,7 @@ function Testimonials (){
 <div className="reviewCardContainer  overflow-hidden flex  w-full h-[380px]   max-w-[350px]  lg:max-w-[700px]">
 {/* review slider */}
 
-    <div style={{marginLeft:-index*350+"px"}} className=' reviewSlider transition-all  w-full flex'>
+    <div style={{marginLeft:-index*350+"px"}} className='reviewSlider transition-all  w-full flex'>
          <div className='card bg-white flex flex-col items-center justify-center gap-3  min-w-[350px] max-w-[350px] h-full p-5 rounded-lg'>
  
  <div className="flex flex-col items-center">
@@ -102,8 +102,6 @@ function Testimonials (){
   <div className='flex gap-2'>
       <FontAwesomeIcon icon={faFacebook} color='black' size='md' />
       <FontAwesomeIcon icon={faLinkedin} color='black' size='md' />
-
-
   </div>
          </div>
          <div className='testimonialCtard bg-white flex flex-col items-center justify-center gap-3  min-w-[350px] max-w-[350px]  h-full p-5 rounded-lg'>
@@ -165,12 +163,12 @@ setInput({...input,[data.name]:data.value})
 }
 function TrustedBy(){
   return(<>
-    <div className=' trustedBy w-full flex-col gap-10 flex justify-center items-center'>
-    <Team />
+    <div className=' trustedBy w-full flex md:flex-row flex-col  items-center pl-[20px] justify-center gap-5 py-10'>
+   
 
-      <div className='font-bold text-customTextColor'>TRUSTED BY:</div>
+      <div className='font-bold text-customTextColor '>TRUSTED BY:</div>
 
-      <div className='flex flex-wrap' >
+      <div className='flex flex-wrap ' >
       <div className='w-[70px] h-[70px] rounded '> <img src="../assets/photo/brands3.png" alt="" srcset="" /></div>
       <div className='w-[70px] h-[70px] rounded'> <img src="../assets/photo/brands2.png" alt="" srcset="" /></div>
       <div className='w-[70px] h-[70px] rounded'> <img src="../assets/photo/brands.png" alt="" srcset="" /></div>
@@ -178,34 +176,82 @@ function TrustedBy(){
       <div className='w-[70px] h-[70px] rounded'> <img src="../assets/photo/brands2.png" alt="" srcset="" /></div>
 
       </div>
-<ProjectSlider />
 
     </div>
 
   </>)
 }
+
 function ProjectSlider() {
   const [index, setIndex] = useState(0);
-  const [width, setWidth] = useState(0);
-  const [projects,setProjects]=useState(
-    [{
-id:"123",
+  const [width, setWidth] = useState(250);
+  const [dots,setDots]=useState('');
+  const [slides,setSlides]=useState(
+    [
+      {
+id:"1",
 heading:"Demo",
-image_path:"../assets/projects/project1.jpg"
-  }])
-  const sliderContainer = useRef(null);
-  const slideWidth = 262; 
-  const totalSilde=6
-  let slidePage=totalSilde/Math.floor(width/slideWidth)
+image_path:"/assets/projects/project1.jpg",
+content:"Demo" 
+  },
+  {
+    id:"2",
+    heading:"Demo",
+    image_path:"/assets/projects/project1.jpg",
+    content:"Demo" 
+      },{
+        id:"3",
+        heading:"Demo",
+        image_path:"/assets/projects/project1.jpg",
+        content:"Demo" 
+          },
+          {
+            id:"4",
+            heading:"Demo",
+            image_path:"/assets/projects/project1.jpg",
+            content:"Demo" 
+              },{
+                id:"5",
+                heading:"Demo",
+                image_path:"/assets/projects/project1.jpg",
+                content:"Demo" 
+                  },
+                  {
+                    id:"6",
+                    heading:"Demo",
+                    image_path:"/assets/projects/project1.jpg",
+                    content:"Demo" 
+                      },{
+                        id:"7",
+                        heading:"Demo",
+                        image_path:"/assets/projects/project1.jpg",
+                        content:"Demo" 
+                          },
+  
+              
+]);
 
+  const sliderContainer = useRef(null);
+ 
+  const slideWidth = 250; 
+  let currentlyShowingItems=Math.floor(width/slideWidth);
+  let slidePage=slides.length/currentlyShowingItems;
+
+ 
+   
+  
+
+  
+  
   const clickHandler = (dir) => {
     if (dir === "left") {
       if (index > 0) setIndex((prev) => prev - 1);
     } else {
-      if (index < slidePage) setIndex((prev) => prev + 1); // 6 is the total number of items in this example
+      if (index <slides.length-currentlyShowingItems) setIndex((prev) => prev + 1); // 6 is the total number of items in this example
     }
   };
 
+ 
   useEffect(() => {
     // Adjusted for gap and other factors
     const resizeObserver = new ResizeObserver((entries) => {
@@ -215,7 +261,8 @@ image_path:"../assets/projects/project1.jpg"
             setWidth(newWidth)
         }
     });
-// 
+
+
     resizeObserver.observe(sliderContainer.current);
     return () => resizeObserver.unobserve(sliderContainer.current);
     ;
@@ -225,52 +272,72 @@ image_path:"../assets/projects/project1.jpg"
     <div className='projectSlider flex flex-col w-full   p-[20px] py-5 items-center gap-5'>
       <div className='text-2xl mr-auto text-white'>Projects</div>
 
-      <div className='relative flex items-center justify-center  w-full '>
+      <div className='relative flex  w-full '>
+      
+        <div className='w-[50px] min-h-full flex justify-center items-center '>
         <div 
           onClick={() => clickHandler("left")} 
           className='p-3   rounded-full flex justify-center w-[40px] items-center font-bold shadow-lg bg-white'>
           <FontAwesomeIcon icon={faArrowLeft} size='sm' />
         </div>
+        </div>
 
-        <div ref={sliderContainer}  className='flex justify-center w-full '>
-          <div style={{ width:width + "px" }} className=' overflow-hidden flex   h-[150px]'>
-            <div 
-            style={{ marginLeft: -index * 272+ "px" }} 
-              className='reviewSlider transition-all w-full gap-3  flex '>
-              <div className='slide flex flex-col items-center border justify-center gap-3 min-w-[260px] '>
-              <img src="../assets/projects/project1.jpg" className='w-full h-full object-cover' alt="" srcset="" />
-              </div>              <div className='slide flex flex-col items-center border justify-center gap-3 min-w-[260px] '>
-              <img src="../assets/projects/project1.jpg" className='w-full h-full object-cover' alt="" srcset="" />
-              </div>
-              <div className='slide flex flex-col items-center border justify-center gap-3 min-w-[260px] '>
-              <img src="../assets/projects/project1.jpg" className='w-full h-full object-cover' alt="" srcset="" />
-              </div>
-              <div className='slide flex flex-col items-center border justify-center gap-3 min-w-[260px] '>
-              <img src="../assets/projects/project1.jpg" className='w-full h-full object-cover' alt="" srcset="" />
-              </div>   <div className='slide flex flex-col items-center border justify-center gap-3 min-w-[260px] '>
-              <img src="../assets/projects/project1.jpg" className='w-full h-full object-cover' alt="" srcset="" />
-              </div>
+        <div ref={sliderContainer}  className='flex justify-center w-full overflow-hidden   '>
+          <div style={{ width:width+12+"px" }} className=' flex overflow-hidden  items-center  h-[160px] py-4 '>
+            <div style={{ marginLeft: -index * 262+ "px" }} className='reviewSlider gap-3 transition-all w-full flex '>
 
+             {
+
+              slides.map((element)=>
+                <div  id={element.id}  className='slide flex items-center justify-center  min-w-[250px] '>
+                 <div className='slide-3d'>
+          {element.id}
+                    {/* <img  src={element.image_path} className='w-full h-full  object-cover absolute' alt="" srcset="" /> */}
+                </div>
+               </div>         
+       
+                )
+             }
+          
             </div>
           </div>
         </div>
 
+        <div className='w-[50px] flex justify-center items-center'>
         <div 
           onClick={() => clickHandler("right")} 
-          className='p-3  flex justify-center items-center rounded-full bg-white shadow-lg'>
-          <FontAwesomeIcon icon={faArrowRight} size='sm' />
+          className='p-3   rounded-full flex justify-center mitems-center font-bold shadow-lg bg-white'>
+          <FontAwesomeIcon icon={faArrowLeft} size='sm' />
         </div>
+        </div>
+
       </div>
+
+      <div className='dots flex'>
+      {
+
+      Array.from({length:slides.length }, (_, i) => (
+          <span
+            key={i}
+            className={`dot ${i==index?"active":""}`}
+          ></span>
+        ))}
+      </div>
+
+
       <div className='w-full min-h-[400px] text-white py-10'>
-<div className='text-xl'> Selected Project Description</div>
+       <div className='text-xl'> Selected Project Description</div>
 
       </div>
     </div>
   );
 }
+
+
+
 function Team(){
   return(<>
-  <div className='flex flex-col w-full justify-center items-center pt-10'>
+  <div className='team flex flex-col w-full justify-center items-center pt-10'>
     <div className='text-2xl text-white'>TEAMS</div>
     <div className='gap-3 p-2 rounded-lg flex'>
       
@@ -307,6 +374,7 @@ function Team(){
 
 
 </div>
+<TrustedBy />
   </div>
 
 
@@ -334,10 +402,28 @@ function GraduationCapIcon(props) {
   )
 }
 function AboutMe(){
-  
+   const heroText=useRef(null)
  const useDis = useDispatch();
  const screenSize = useSelector((states)=> states.windowSize);
 
+
+      const moveHandler =(e) => {
+            const textRect =   heroText.current.getBoundingClientRect();
+            const textX = e.clientX - textRect.left;
+            const textY = e.clientY - textRect.top;
+
+            const centerX = textRect.width / 2;
+            const centerY = textRect.height / 2;
+
+            const rotateX = (centerY - textY) / 15;
+            const rotateY = (textX - centerX) / 15;
+
+            heroText.current.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+      }
+
+       const leaveHandler= () => {
+          heroText.current.style.transform = `rotateX(0) rotateY(0)`;
+        };
 
 useEffect(()=>{
   const handler=()=>{
@@ -353,7 +439,7 @@ return (
 <>
 
 <div className={` relative  flex flex-col w-full  `}>
-  <section className=' relative p-0 m-0  flex flex-col w-full h-screen max-h-[1200px] ' >
+  <section className=' relative p-0 m-0 hero flex flex-col w-full h-screen max-h-[1200px] ' >
   
 
 <NavBar />
@@ -362,10 +448,10 @@ return (
       <div  className={`flex ${  screenSize[0].width>=768 ?(screenSize[0].height<=700?'flex-row':"flex-row items-center") :'flex-col'}  w-full flex-1  relative  grid-lines`}>
         {/*^ Responsive swicth between flex-row and flex-col depending on width and height^ */}
 
-     <section className= {`max-w-[600px] w-full  pl-[20px] ${screenSize[0].height<=700?'h-full justify-center ':' h-[500px] justify-end' }   flex flex-col `}>
-        <div className="relative text-white 
+     <section className= {`max-w-[600px] w-full  pl-[20px] ${screenSize[0].height<=700?'h-full justify-center ':' h-[400px] justify-end' }   flex flex-col `}>
+        <div  className="relative  text-container text-white 
           max-w-[400px] my-5 " > 
-            <div className='headingAboutMe lg:text-3xl text-2xl tracking-wider  font-semibold '> 
+            <div ref={heroText} onMouseMove={moveHandler} onMouseLeave={leaveHandler} className='headingAboutMe text-3d lg:text-4xl text-3xl tracking-wider  font-bold'> 
             Hi, This is <span> </span>  
              <span className='text-red-500'>T</span>
             <span className='text-yellow-400'>a</span>
@@ -389,17 +475,17 @@ and user-friendly solutions. Let's bring your idea into reality.
 </>
 }
 </div>
-<div className=' follow text-sm  text-white  flex h-[50px]   mt-[20px]   '>
-  <div className='relative w-[50px] h-full flex justify-center items-center  '> 
+<div className='  text-sm  text-white  flex h-[50px]  gap-5  mt-[20px]   '>
+  <div className='relative follow h-full flex justify-center items-center  '> 
      <FontAwesomeIcon className='icon-hover' icon={faFacebook} size='2xl' />
   </div>
-  <div className='relative w-[50px] h-full flex justify-center items-center  ' > 
+  <div className='relative  follow h-full flex justify-center items-center  ' > 
      <FontAwesomeIcon className='icon-hover' icon={faLinkedin}  size='2xl' />
   </div>
-  <div className='relative w-[50px] h-full flex justify-center items-center  '> 
+  <div className='relative  follow h-full flex justify-center items-center  '> 
       <FontAwesomeIcon className='icon-hover' icon={faTwitter}  size='2xl'  />
   </div>
-  <div className='relative w-[50px] h-full flex justify-center items-center '> 
+  <div className='relative  follow h-full flex justify-center items-center '> 
      <FontAwesomeIcon className='icon-hover' icon={faGithub}  size='2xl'  />
   </div> 
 </div>
@@ -430,36 +516,39 @@ and user-friendly solutions. Let's bring your idea into reality.
     </div>
   </section>
 <div className='education  w-full  flex flex-col '>
-  <div className='text-2xl py-3 pl-[20px]  text-customTextColor flex items-center '> 
-  {/* <FontAwesomeIcon icon={faGraduationCap} className='' size='lg' /> */}
-  <GraduationCapIcon className="w-8 h-8 mr-2" />
-  
-    <span className="ml-2 py-10 ">Educational Qualification</span>
+
+
+  <section className='flex  flex-col w-full gap-3 py-[40px]  relative '>
+ 
+  <div className='text-xl bg-white ml-[20px] customBorderRadius z-[100] flex gap-5 w-[300px] pl-4 py-2 items-center  '> 
+    <span className=" flex  items-center gap-2"> <GraduationCapIcon /> Educational Qualification</span>
   </div>
-
-  <section className='flex flex-col lg:flex-row pl-[20px] w-full bg-white  justify-center items-center '>
-
-  <div className="flex inset-0  w-full h-full shadow-md items-center    "> 
-    <div className='flex gap-5  pl-5 rounded-md  w-full  max-w-[500px]  items-center  h-[200px] ' >
+  <div className='flex flex-col lg:flex-row px-5 z-[100]    gap-2 w-full justify-center items-center '>
+{/* Academic */}
+  <div className="flex inset-0 z-[100] rounded-sm w-full  h-[230px] shadow-md    items-center    "> 
+    <div className='flex gap-5  w-full  max-w-[500px]  items-center  ' >
     <div className='myPhoto w-[150px] h-[150px] bg-white rounded-full overflow-hidden'>
     <img src="../assets/photo/heroPhoto.png" className='w-full h-full object-cover' alt="" srcset="" />
     </div>
-    <div className='flex flex-col gap-3'>
-    <div className='heading text-xl  '>Academic Education </div>
+    <div className='flex flex-col gap-3 text-white'>
+    <div className='heading text-lg  '>Academic Education </div>
       <ul className=' text-sm '>
-        <li>Studied: BBA (Business Administrative)</li>
+        <li>Studied: BBA </li>
         <li>Major Subject: Accounting</li>
         <li>Institute: National University Of Bangladesh</li>
-      </ul>[]
+      </ul>
     </div>
     </div>
   </div>
-<div className='flex flex-col w-full h-full shadow-xl  p-5   justify-center '>
+{/* MERN */}
+<div className='flex flex-col w-full  shadow-md rounded-sm  md:min-w-[600px]  p-5 justify-center '>
  
-      <div className='flex customShadow  md:flex-row flex-col  md:h-[200px] w-full  gap-3 py-5'>
-      <div className=' bg-white border rounded-md  flex-wrap w-full max-w-[300px] h-[150px] '></div>
+  <div className='flex   md:flex-row flex-col  w-full   gap-3'>
+  <div className=' bg-white border rounded-md  flex-wrap min-w-[200px] object-cover max-w-[300px] h-[190px] '>
+    <img src="../assets/fakeCertificate.jpeg" className=' w-full h-full' alt="" srcset="" />
+  </div>
 
-  <div className='  text-sm py-2 lg:pl-2'>
+  <div className='text-white   text-sm py-2 lg:pl-2'>
   <h1 className=' text-xl pb-5 '> MERN Stack Course - Interactive Care</h1>
   I successfully completed an intensive course on the MERN stack from Interactive Care.
   This course provided comprehensive training on the full-stack development process using MongoDB, Express.js, React, and Node.js.
@@ -467,14 +556,20 @@ and user-friendly solutions. Let's bring your idea into reality.
   </div> 
   </div>
 
-  </section>
+  </div>
+  {/* <div className="orb top-[50px] left-[50%]"></div>
+  <div className="orb bottom-[30px]"></div>
+  <div className="orb bottom-[0%] left-[70%]"></div> */}
 
-  <div className='coveredTopics pl-[20px] py-5 flex flex-wrap justify-center md:justify-normal gap-5 '>
- 
- <div className='text-xl flex justify-start md:justify-start text-customTextColor w-full items-center gap-2 '>
- <FontAwesomeIcon icon={faTasks} className='' size='lg' />
- Key Topics Covered
+  </section>
+<div>
+<div className='text-xl flex justify-start md:justify-start   mb-auto w-full items-center gap-2 '>
+ <div className='customBorderRadius ml-[20px] flex gap-5 w-[300px] pl-4 py-2 bg-[#ebe8e8]'>
+   <FontAwesomeIcon icon={faTasks} className='' size='lg' />
+   Key Topics Covered</div>
  </div>
+<div>
+<div className='coveredTopics pl-[20px] py-5 flex  flex-wrap justify-center gap-5 md:justify-normal'>
 
 <div className='card-item rounded-lg flex flex-col w-full max-w-[500px]  md:w-[350px] shadow-lg p-5'>
  <div className='heading text-customTextColor flex justify-between lg:text-xl md:text-md text-md tracking-wider '>
@@ -580,11 +675,17 @@ and user-friendly solutions. Let's bring your idea into reality.
 </div>
 
   </div>
-  <TrustedBy />
+
+</div>
+
+</div>
+<Team />
+  <ProjectSlider />
+
+
 </div>
 <Testimonials />
 <ContactForm />
-
 </div>
 
 </>
